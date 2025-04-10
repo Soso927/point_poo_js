4e6d24214e8ad28248af99c013d9664aeff6aea6
sofiane <sofianeagoumellah1@gmail.com>
2025-04-10 08:49:17 +0200

    fin de l'exercice et ajout des commentaires

--- js/classes/Point.js
+++ js/classes/Point.js
@@ -1,34 +1,37 @@
 // Vous mettrez ici le code de la classe Point
+
+// Déclaration de la classe Point
 class Point {
+    // Déclaration des attributs privés x et y (grâce au #)
     #x;
     #y;
     
-    constructor (abscisse,ordonnee){
+    // Constructeur appelé lors de l’instanciation de la classe
+    constructor (abscisse, ordonnee) {
+        // Initialisation de l'attribut privé #x avec la valeur passée en paramètre
         this.#x = abscisse;
+
+        // Initialisation de l'attribut privé #y avec la valeur passée en paramètre
         this.#y = ordonnee;
     }
 
-    get x ()
-    {
+    // Getter pour l'attribut privé #x (abscisse)
+    get x() {
         return this.#x;
     }
 
-    set x (abscisse)
-    {
+    // Setter pour modifier la valeur de l'attribut privé #x (abscisse)
+    set x(abscisse) {
         this.#x = abscisse;
     }
 
-
-    get y ()
-    {
+    // Getter pour l'attribut privé #y (ordonnee)
+    get y() {
         return this.#y;
     }
 
-    set y (ordonnee)
-    {
+    // Setter pour modifier la valeur de l'attribut privé #y (ordonnee)
+    set y(ordonnee) {
         this.#y = ordonnee;
     }
-
 }
-
-
--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 // Vous mettrez ici le code JavaScript principal
+
+// On sélectionne le bouton dans le document HTML
 const bouton = document.querySelector('button');
+
+// On sélectionne l'élément où le résultat sera affiché (par ID)
 const resultat = document.querySelector('#point_state');
-bouton.addEventListener('click', ()=> {
-const x = document.querySelector('#x').value;
-const y = document.querySelector('#y').value;
-// j'instancie la classe point avec le constructeur
-let abscisse = new Point(x,y);
-console.log(`Le résultat de l'abscisse est ${abscisse.x}`);
-console.log(`le résultat de l'abscisse est ${abscisse.y}`);
-// resultat.textContent = `Point A (${x} ; ${y})`;
 
+// Affichage de l'élément dans la console pour vérification
+console.log(resultat);
 
+// Ajout d'un événement sur le bouton : quand on clique, on exécute la fonction
+bouton.addEventListener('click', () => {
+    // On récupère la valeur entrée dans le champ d’input avec l’ID x
+    const x = document.querySelector('#x').value;
 
-})
+    // On récupère la valeur entrée dans le champ d’input avec l’ID y
+    const y = document.querySelector('#y').value;
 
+    // On crée une nouvelle instance de la classe Point avec les coordonnées x et y
+    let point = new Point(x, y);
 
+    // Affiche l’abscisse (x) du point dans la console
+    console.log(`Le résultat de l'abscisse est ${point.x}`);
 
+    // Affiche l’ordonnée (y) du point dans la console (attention : le texte parle encore d’abscisse par erreur)
+    console.log(`le résultat de l'abscisse est ${point.y}`);
 
-// let point = new Point (15,12)
-// point.x = 32;
-// console.log(point);
+    // Affiche les coordonnées du point dans l’élément HTML ciblé
+    resultat.textContent = `Point A (${point.x} ; ${point.y})`;
+});
 
